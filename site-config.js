699bfe11845c601042883de7e7cbae337c130d3a
BetterThanAwesome <betterthanawesome2020@gmail.com>
2026-09-23 19:26:55 -0600

    Point portfolio YouTube link to work playlist

--- site-config.js
+++ site-config.js
@@ -16,7 +16,7 @@ window.SITE_CONFIG = {
     bookMarketing: "/book-marketing/",
     generalEmail: "mailto:Hello@MADvertising.agency?subject=MIKEDENMAN.ONLINE%20INQUIRY%20%7C%20New%20Project",
     bookMarketingEmail: "mailto:Hello@MADvertising.agency?subject=BOOK%20MARKETING%20INQUIRY%20%7C%20MikeDenman.Online&body=Hi%20Mike%2C%0D%0A%0D%0AI%20found%20your%20book%20advertising%20services%20through%20MikeDenman.Online.%0D%0A%0D%0ABook%20%2F%20audiobook%20title%3A%0D%0AAuthor%20%2F%20publisher%3A%0D%0AIs%20it%20published%20and%20available%20to%20purchase%20online%3F%20Yes%20%2F%20No%0D%0APurchase%20link%3A%0D%0AFormat%28s%29%3A%0D%0APrimary%20goal%3A%0D%0AAdvertising%20creative%20available%3F%20Yes%20%2F%20No%0D%0ABudget%20available%20for%20the%20first%2030%20days%20%28starting%20at%20%24555%29%3A%0D%0AAnything%20else%20I%20should%20know%3A%0D%0A%0D%0AThanks%2C",
-    youtube: "https://www.youtube.com/results?search_query=Mike+Denman+film+marketing",
+    youtube: "https://youtube.com/playlist?list=PL99611941E9643AB3&si=GoACwEczbCr-9G5p",
     wbGames: "https://www.warnerbrosgames.com/",
     avalanche: "https://www.avalanchestudios.com/",
     snowblind: "https://en.wikipedia.org/wiki/Snowblind_Studios",
